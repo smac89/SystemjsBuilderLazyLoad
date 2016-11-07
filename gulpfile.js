c28550d11302bcec16376f0b5ae59a7d6bd4ac84
abidmix <abidmix@gmail.com>
2016-11-07 14:43:24 -0500

    first added files

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+﻿"use strict";
+var gulp = require('gulp');
+var Builder = require('systemjs-builder');
+var path = require("path");
+var config = {
+  transpiler: 'babel',
+  typescriptOptions: {
+    module: 'cjs'
+  },
+  map: {
+    typescript: 'node_modules/typescript/lib/typescript.js',
+    "@angular": "node_modules/@angular",
+    "rxjs": "node_modules/rxjs"
+  },
+  paths: {
+    '*': '*.js'
+  },
+  meta: {
+    'node_modules/@angular/*': { build: false },
+    'node_modules/rxjs/*': { build: false }
+  },
+};
+
+
+gulp.task('appbundle', function() {
+    var builder = new Builder();
+    builder.loadConfigSync('./systemjs.config.js')
+     builder.config(config);
+    builder.buildStatic('build/app.module','bundled/app.module.min.js',{
+        minify: true,
+        mangle: false
+    })
+});
+
+
+
+ 
